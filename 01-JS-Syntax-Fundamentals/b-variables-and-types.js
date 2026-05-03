e4722c91482585957e2ccaec49ab9f7e3f7b1ae5
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2026-05-03 19:49:23 +0300

    js

---
+++ 01-JS-Syntax-Fundamentals/b-variables-and-types.js
@@ -0,0 +1,6 @@
+// Variables and block scope
+let firstName = 'Pesho'; // Block scope
+var lastName = 'Petrov'; // Function scope
+const middleName = 'Ivanov'; // Cannot be reassigned / Block scope
+
+
