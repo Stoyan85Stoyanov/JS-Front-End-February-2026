11936fd16376b2e31457c1f1c85845133289ea5a
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2026-05-27 20:36:20 +0300

    js

---
+++ Exercise_1/05.js
@@ -0,0 +1,10 @@
+function multiplicationTable(number) {
+
+    for (let n = 1; n <= 10; n++) {
+        console.log(`${number} X ${n} = ${number * n}`);      
+    }
+
+}
+
+multiplicationTable(5);
+multiplicationTable(2);
