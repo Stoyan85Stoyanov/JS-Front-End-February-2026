daf81fafebda824b81646af6be41cc0b4d4069b1
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2026-05-27 20:36:32 +0300

    js

---
+++ Exercise_1/08.js
@@ -0,0 +1,14 @@
+function reversedChars(a, b, c) {
+    
+    const arr = [a, b, c];
+    arr.reverse();
+    console.log(arr.join(" "));
+     
+}
+
+//    const result = `${c} ${b} ${a}`;
+//     console.log(result);
+
+
+reversedChars('A','B','C');
+reversedChars('1','L','&');
