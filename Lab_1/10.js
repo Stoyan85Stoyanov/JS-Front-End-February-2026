c0c4bce28e8c92cd99ef9ca7669c6dfd8df39f34
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2026-06-19 19:26:19 +0300

    js

---
+++ Lab_1/10.js
@@ -0,0 +1,9 @@
+function numbersFromM_To_N(bigNumber, smallNumber) {
+     
+    for (let num = bigNumber; num >= smallNumber; num--) {
+        console.log(num);  
+    }
+}
+
+numbersFromM_To_N(6, 2);
+numbersFromM_To_N(4, 1);
