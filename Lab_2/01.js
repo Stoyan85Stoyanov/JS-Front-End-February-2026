d06ea6f1aa431da7d32f72852c6b50abde6227d2
StoyanStoyanov085 <nikolay.stoyanov.kz@mail.bg>
2026-06-20 20:38:36 +0300

    js

---
+++ Lab_2/01.js
@@ -0,0 +1,12 @@
+function sumFirstAndLastArrayElements(numbers) {
+
+    let firstElement = numbers[0];
+    let lastElement = numbers[numbers.length - 1];
+
+    console.log(firstElement + lastElement);
+    
+}
+
+sumFirstAndLastArrayElements([20, 30, 40]);
+sumFirstAndLastArrayElements([10, 17, 22, 33]);
+sumFirstAndLastArrayElements([11, 58, 69]);
