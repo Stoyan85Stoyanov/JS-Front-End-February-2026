e89dc4d2f080619cdec114146202639ef1ebab67
StoyanStoyanov085 <nikolay.stoyanov.kz@mail.bg>
2026-06-20 20:38:49 +0300

    js

---
+++ Lab_2/04.js
@@ -0,0 +1,8 @@
+function substring(str, startIndex, count) {
+
+    console.log(str.substring(startIndex, count + startIndex));
+    
+}
+
+substring('ASentence', 1, 8);
+substring('SkipWord', 4, 7);
