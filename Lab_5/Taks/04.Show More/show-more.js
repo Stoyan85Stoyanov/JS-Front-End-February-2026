84ef4c62258b7a61d1f6fc1614a824a96cf9c59d
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-06-30 18:41:37 +0300

    js

---
+++ Lab_5/Taks/04.Show More/show-more.js
@@ -0,0 +1,9 @@
+function showText() {
+    
+    let moreAElement = document.getElementById('more');
+    let spanElement = document.getElementById('text');
+
+    moreAElement.style.display = 'none';
+    spanElement.style.display = 'inline';  // на един ред
+    // spanEl.style.display = 'block'; // на следващ ред 
+}
