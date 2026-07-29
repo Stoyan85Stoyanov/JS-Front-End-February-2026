d4c57c81888335fcf64d8cebb29fa96c40bb0fb0
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-07-29 18:30:33 +0300

    js

--- Lab_1/03.js
+++ Lab_1/03.js
@@ -9,28 +9,28 @@
 // }
 
 
-function excellentGrade(grade) {
-    if (grade >= 5.50) {
-        console.log("Excellent");
-        return;
-    }
-
-    console.log("Not excellent");
-}
-
-
 // function excellentGrade(grade) {
-//     let result;
-
 //     if (grade >= 5.50) {
-//         result = "Excellent";
-
-//     } else {
-//         result = "Not excellent";
+//         console.log("Excellent");
+//         return;
 //     }
 
-//     console.log(result);
+//     console.log("Not excellent");
 // }
+
+
+function excellentGrade(grade) {
+    let result;
+
+    if (grade >= 5.50) {
+        result = "Excellent";
+
+    } else {
+        result = "Not excellent";
+    }
+
+    console.log(result);
+}
 
 
 excellentGrade(5.50);
