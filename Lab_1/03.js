27095a8697615540ebf36b358e27d3a0c069ce8e
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-07-29 18:29:56 +0300

    js

--- Lab_1/03.js
+++ Lab_1/03.js
@@ -1,12 +1,37 @@
-function excellentGrade(grade) {
+// function excellentGrade(grade) {
     
-    if(grade >= 5.50){
-      console.log("Excellent");  
+//     if(grade >= 5.50){
+//       console.log("Excellent");  
 
-    }else{
-        console.log("Not excellent");
-    }   
+//     }else{
+//         console.log("Not excellent");
+//     }   
+// }
+
+
+function excellentGrade(grade) {
+    if (grade >= 5.50) {
+        console.log("Excellent");
+        return;
+    }
+
+    console.log("Not excellent");
 }
+
+
+// function excellentGrade(grade) {
+//     let result;
+
+//     if (grade >= 5.50) {
+//         result = "Excellent";
+
+//     } else {
+//         result = "Not excellent";
+//     }
+
+//     console.log(result);
+// }
+
 
 excellentGrade(5.50);
 excellentGrade(4.35);
