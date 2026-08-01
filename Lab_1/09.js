10954622e28606f3c3e09faba6c3d9248719cb56
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-08-01 14:58:32 +0300

    js

--- Lab_1/09.js
+++ Lab_1/09.js
@@ -1,8 +1,18 @@
-function numbersFrom_1To_5() {
+// function numbersFrom_1To_5() {
      
-    for (let num = 1; num <= 5; num++) {
-        console.log(num);  
-    }
+//     for (let num = 1; num <= 5; num++) {
+//         console.log(num);  
+//     }
+// }
+
+function numbersFrom_1To_5() {
+
+    let number = 1;
+     
+   while(number <= 5) {
+     console.log(number);  
+     number++;
+   }   
 }
 
 numbersFrom_1To_5();
