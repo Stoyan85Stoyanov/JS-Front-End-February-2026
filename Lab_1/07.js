a37bd5966fe00bd3773ec25c805d88ee3e5e9679
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-08-02 12:57:30 +0300

    js

--- Lab_1/07.js
+++ Lab_1/07.js
@@ -1,68 +1,114 @@
+// function theatrePromotions(dayType, age) {
+
+//     let price = 0;
+
+//     switch (dayType) {
+//         case 'Weekday':
+
+//             if (age >= 0 && age <= 18) {
+//                 price = 12;
+
+//             } else if (age > 18 && age <= 64) {
+//                 price = 18;
+
+//             } else if (age > 64 && age <= 122) {
+//                 price = 12;
+
+//             } else {
+//                 console.log(`Error!`);
+//                 return;
+//             }
+
+//             break;
+
+
+//         case 'Weekend':
+
+//             if (age >= 0 && age <= 18) {
+//                 price = 15;
+
+//             } else if (age > 18 && age <= 64) {
+//                 price = 20;
+
+//             } else if (age > 64 && age <= 122) {
+//                 price = 15;
+
+//             } else {
+//                 console.log(`Error!`);
+//                 return;
+//             }
+
+//             break;
+
+
+//         case 'Holiday':
+
+//             if (age >= 0 && age <= 18) {
+//                 price = 5;
+
+//             } else if (age > 18 && age <= 64) {
+//                 price = 12;
+
+//             } else if (age > 64 && age <= 122) {
+//                 price = 10;
+
+//             } else {
+//                 console.log(`Error!`);
+//                 return;
+//             }
+
+//             break;
+
+//     }
+//     console.log(`${price}$`);
+    
+// }
+
+
+
 function theatrePromotions(dayType, age) {
+
+    if (age < 0 || age > 122) {
+        console.log("Error!");
+        return;
+    }
 
     let price = 0;
 
-    switch (dayType) {
-        case 'Weekday':
+    if (dayType === "Weekday") {
 
-            if (age >= 0 && age <= 18) {
-                price = 12;
+        if (age <= 18 || age > 64) {
+            price = 12;
 
-            } else if (age > 18 && age <= 64) {
-                price = 18;
+        } else {
+            price = 18;
+        }
 
-            } else if (age > 64 && age <= 122) {
-                price = 12;
+    } else if (dayType === "Weekend") {
 
-            } else {
-                console.log(`Error!`);
-                return;
-            }
+        if (age <= 18 || age > 64) {
+            price = 15;
 
-            break;
+        } else {
+            price = 20;
+        }
 
+    } else if (dayType === "Holiday") {
 
-        case 'Weekend':
+        if (age <= 18) {
+            price = 5;
 
-            if (age >= 0 && age <= 18) {
-                price = 15;
-
-            } else if (age > 18 && age <= 64) {
-                price = 20;
-
-            } else if (age > 64 && age <= 122) {
-                price = 15;
-
-            } else {
-                console.log(`Error!`);
-                return;
-            }
-
-            break;
-
-
-        case 'Holiday':
-
-            if (age >= 0 && age <= 18) {
-                price = 5;
-
-            } else if (age > 18 && age <= 64) {
-                price = 12;
-
-            } else if (age > 64 && age <= 122) {
-                price = 10;
-
-            } else {
-                console.log(`Error!`);
-                return;
-            }
-
-            break;
-
+        } else if (age <= 64) {
+            price = 12;
+            
+        } else {
+            price = 10;
+        }
     }
-    console.log(`${price}$`);
-    
+
+    console.log(price + "$");
 }
+
 
 theatrePromotions('Weekday', 42);
 theatrePromotions('Holiday', -12);
