af94444884df36a4ff6673f3046d238139f49cc4
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-08-11 09:46:20 +0300

    js

--- Lab_2/02.js
+++ Lab_2/02.js
@@ -1,8 +1,19 @@
-function reverseAnArrayOfNumbers(n, numbers) {
+// function reverseAnArrayOfNumbers(n, numbers) {
 
-    let finalNumbers = numbers.slice(0, n).reverse();
-    console.log(finalNumbers.join(' '));
+//     let finalNumbers = numbers.slice(0, n).reverse();
+//     console.log(finalNumbers.join(' '));
     
+// }
+
+function reverseAnArrayOfNumbers(number, numbers) {
+
+    let result = [];
+
+    for (let i = number - 1; i >= 0; i--) {
+        result.push(numbers[i]);
+    }
+
+    console.log(result.join(' '));
 }
 
 reverseAnArrayOfNumbers(3, [10, 20, 30, 40, 50]);
