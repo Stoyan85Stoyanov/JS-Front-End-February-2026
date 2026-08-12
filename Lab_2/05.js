481e74e98f9a107355f937529e8298156a3746a5
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-08-12 21:03:36 +0300

    js

--- Lab_2/05.js
+++ Lab_2/05.js
@@ -1,9 +1,15 @@
-function censoredWords(text, text_) {
+// function censoredWords(text, text_) {
 
-    let starTemplate = '*'.repeat(text_.length);
-    text = text.replaceAll(text_, starTemplate);
-    console.log(text);
+//     let starTemplate = '*'.repeat(text_.length);
+//     text = text.replaceAll(text_, starTemplate);
+//     console.log(text);
     
+// }
+
+function censoredWords(text, word) {
+    
+    text = text.replaceAll(word, '*'.repeat(word.length));
+    console.log(text);
 }
 
 censoredWords('A small sentence with some words', 'small');
