22b1dd3443245e7f36b626574ab2ed9de296aba2
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-08-14 09:05:12 +0300

    js

---
+++ Lab_1/demo_1.js
@@ -0,0 +1,32 @@
+// // Function Declaration -> извиква се веднъж
+// function calculate(firstNumber, secondNumber) {
+//     console.log(firstNumber + secondNumber);
+// }
+
+// // Function Invocation -> може да се извиква повече от веднъж
+// calculate(5, 5);
+// calculate(10, 5);
+
+// 10
+// 15
+
+//----------------------------------------
+
+
+function calculate(firstNumber, secondNumber) {
+    let sumResult = firstNumber + secondNumber;
+
+
+    // console.log('The result is: ', result);
+    // console.log('The result is: '+ result);
+
+    // String Interpolation (Template String)
+    console.log(`The result of ${firstNumber} and ${secondNumber} is equal to ${sumResult.toFixed(2)}!`);   // За извличане на информация Ctrl+Shift+Space
+}
+
+calculate(5, 5);
+calculate(10, 5);
+
+// The result is:  10
+// The result is:  15
+
