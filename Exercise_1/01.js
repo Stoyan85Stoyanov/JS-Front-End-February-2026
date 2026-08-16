956730d870b4f11591d0211b23d76b491c2f92c6
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-08-16 21:49:06 +0300

    js

--- Exercise_1/01.js
+++ Exercise_1/01.js
@@ -1,24 +1,51 @@
+// function ages(age) {
+
+//     if (age < 0) {
+//         console.log('out of bounds');
+
+//     } else if (age <= 2) {
+//         console.log('baby');
+
+//     } else if (age <= 13) {
+//         console.log('child');
+
+//     } else if (age <= 19) {
+//         console.log('teenager');
+
+//     } else if (age <= 65) {
+//         console.log('adult');
+
+//     } else {
+//         console.log('elder');
+//     }
+// }
+
+
+
 function ages(age) {
 
-    if (age < 0) {
-        console.log('out of bounds');
+    if (age >= 66) {
+        console.log('elder');
 
-    } else if (age <= 2) {
-        console.log('baby');
-
-    } else if (age <= 13) {
-        console.log('child');
-
-    } else if (age <= 19) {
-        console.log('teenager');
-
-    } else if (age <= 65) {
+    } else if (age >= 20) {
         console.log('adult');
 
+    } else if (age >= 14) {
+        console.log('teenager');
+
+    } else if (age >= 3) {
+        console.log('child');
+
+    } else if (age >= 0) {
+        console.log('baby');
+
     } else {
-        console.log('elder');
+        console.log('out of bounds');
+
     }
 }
+
+
 
 ages(20);
 ages(1);
