026032d3d2f224e52905fe2fe15ef3e5efefc482
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-08-18 18:50:26 +0300

    js

--- Exercise_1/04.js
+++ Exercise_1/04.js
@@ -1,14 +1,46 @@
+// function printAndSum(start, end) {
+
+//     let tolalSum = 0;
+//     let output = '';
+
+//     for (let n = start; n <= end; n++) {
+//         tolalSum += n;
+//         output += n + ' ';
+//     }
+
+//     console.log(output);
+//     console.log(`Sum: ${tolalSum}`);
+
+// }
+
+
+// function printAndSum(start, end) {
+
+//     let tolalSum = 0;
+//     let output = '';
+
+//     for (let j = end; j >= start; j--) {
+//         tolalSum += j;
+//         output += j + ' ';
+//     }
+
+//     console.log(output.trim());
+//     console.log(`Sum: ${tolalSum}`);
+
+// }
+
+
 function printAndSum(start, end) {
 
     let tolalSum = 0;
     let output = '';
 
-    for (let n = start; n <= end; n++) {
-        tolalSum += n;
-        output += n + ' ';
+    for (let j = start; j <= end; j++) {
+        tolalSum += j;
+        output += j + ' ';
     }
 
-    console.log(output);
+    console.log(output.trim());
     console.log(`Sum: ${tolalSum}`);
 
 }
