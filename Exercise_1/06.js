bd87203d8fb1b0eb7d76932bcb6e80018f949020
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-08-21 11:17:25 +0300

    js

--- Exercise_1/06.js
+++ Exercise_1/06.js
@@ -1,17 +1,30 @@
+// function sumDigits(number) {
+
+//     let input = number;
+//     let totalSum = 0;
+
+//     while (input > 0) {
+
+//         let remainer = input % 10;
+//         totalSum += remainer;
+//         input = Math.trunc(input / 10);
+//     }
+
+//     console.log(totalSum);   
+// }
+
+
 function sumDigits(number) {
 
-    let input = number;
-    let totalSum = 0;
+    let totalSum = number.toString().split("")
+        .reduce((sum, digit) =>
+            sum + parseInt(digit), 0);
 
-    while (input > 0) {
+    console.log(totalSum);    
 
-        let remainer = input % 10;
-        totalSum += remainer;
-        input = Math.trunc(input / 10);
-    }
-    
-    console.log(totalSum);   
 }
+
+
 
 sumDigits(245678);
 sumDigits(97561);
