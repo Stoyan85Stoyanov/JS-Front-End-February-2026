8377e2f6a6295e02bf8cda5c57116eae57ded19d
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-08-24 22:51:41 +0300

    js

--- Exercise_1/12.js
+++ Exercise_1/12.js
@@ -1,33 +1,69 @@
-function cookingByNumbers(number, operations1, operations2, operations3, operations4, operations5) {
+// function cookingByNumbers(number, operations1, operations2, operations3, operations4, operations5) {
+
+//     let result = Number(number);
+//     let operations = [operations1, operations2, operations3, operations4, operations5];
+
+
+//     for (const op of operations) {
+
+
+//         if (op === "chop") {
+//             result /= 2;
+
+//         } else if (op === "dice") {
+//             result = Math.sqrt(result);
+
+//         } else if (op === "spice") {
+//             result += 1;
+
+//         } else if (op === "bake") {
+//             result *= 3;
+
+//         } else if (op === "fillet") {
+//             result *= 0.8;
+
+//         }
+
+//         console.log(Number(result.toFixed(1)));
+//     }
+// }
+
+
+function cookingByNumbers(number, ...operations) {
 
     let result = Number(number);
-    let operations = [operations1, operations2, operations3, operations4, operations5];
 
+    for (let i = 0; i < operations.length; i++) {
+        const action = operations[i];
 
-    for (const op of operations) {
+        switch (action) {
 
+            case 'chop':
+                result /= 2;
+                break;
 
-        if (op === "chop") {
-            result /= 2;
+            case 'dice':
+                result = Math.sqrt(result);
+                break;
 
-        } else if (op === "dice") {
-            result = Math.sqrt(result);
+            case 'spice':
+                result += 1;
+                break;
 
-        } else if (op === "spice") {
-            result += 1;
-
-        } else if (op === "bake") {
-            result *= 3;
-
-        } else if (op === "fillet") {
-            result *= 0.8;
-
+            case 'bake':
+                result *= 3;
+                break;
+                
+            case 'fillet':
+                result -= result * 0.2;
+                break;
         }
 
-        console.log(Number(result.toFixed(1)));
+        console.log(result);
     }
 }
 
 
 cookingByNumbers('32', 'chop', 'chop', 'chop', 'chop', 'chop');
+console.log('----------------------------------------------');
 cookingByNumbers('9', 'dice', 'spice', 'chop', 'bake', 'fillet');
