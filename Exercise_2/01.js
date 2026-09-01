8acc50bc89247e60988c51e1dad4b1d89a90d9e2
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-09-01 18:30:13 +0300

    js

--- Exercise_2/01.js
+++ Exercise_2/01.js
@@ -1,21 +1,46 @@
+// function arrayRotation(array, numberOfRotations) {
+
+//     numberOfRotations %= array.length; // за по-бързо изпълнение при по-голям брой ротации
+
+//     for(let i = 0; i < numberOfRotations; i++) {
+//         array.push(array.shift());
+//     }
+//     console.log(array.join(" "));  
+
+
+//     // let result = [];
+
+//     // for (let i = 0; i < array.length; i++) {
+//     //     result.push(array[(numberOfRotations + i) % array.length]);
+//     // }
+
+//     //   console.log(result.join(" "));
+// }
+
+
+
+// function arrayRotation(array, numberOfRotations) {
+
+//     for (let i = 0; i < numberOfRotations; i++) {
+//         array.push(array.shift());
+//     }
+
+//     console.log(array.join(" "));
+// }
+
+
 function arrayRotation(array, numberOfRotations) {
+    
+    const n = array.length;
+    numberOfRotations %= n;
 
-    numberOfRotations %= array.length; // за по-бързо изпълнение при по-голям брой ротации
+    let result = array.slice(numberOfRotations).concat(array.slice(0, numberOfRotations));
 
-    for(let i = 0; i < numberOfRotations; i++) {
-        array.push(array.shift());
-    }
-    console.log(array.join(" "));  
-
-
-    // let result = [];
-
-    // for (let i = 0; i < array.length; i++) {
-    //     result.push(array[(numberOfRotations + i) % array.length]);
-    // }
-
-    //   console.log(result.join(" "));
+    console.log(result.join(" "));
 }
+    
+
+    
 
 
 arrayRotation([51, 47, 32, 61, 21], 2);
