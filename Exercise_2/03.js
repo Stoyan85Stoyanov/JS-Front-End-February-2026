0484a15d2a45257b1e83230c4b54bc054e2bd74b
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-09-07 18:29:14 +0300

    js

--- Exercise_2/03.js
+++ Exercise_2/03.js
@@ -1,9 +1,26 @@
-function listOfNames(array) {
-     
-    array.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
-    .forEach((element, i) => console.log(`${i + 1}.${element}`));
+// function listOfNames(array) {
 
+//     array.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+//     .forEach((element, i) => console.log(`${i + 1}.${element}`));
+
+// }
+
+
+// function listOfNames(names) {
+//     names
+//         .sort((a, b) => a.localeCompare(b))
+//         .forEach((name, index) => console.log(`${index + 1}.${name}`));
+// }
+
+function listOfNames(arrayOfNames) {
+
+    arrayOfNames.sort();
+
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        console.log((i + 1) + "." + arrayOfNames[i]);
+    }
 }
+
 
 listOfNames(["John", "Bob", "Christina", "Ema"]);
 
