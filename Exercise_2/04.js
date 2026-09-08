4e5a068f6a790669ddb44c9dd6aff9ebc8ad66f7
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-09-08 21:24:17 +0300

    js

--- Exercise_2/04.js
+++ Exercise_2/04.js
@@ -1,71 +1,112 @@
-function sortingNumbers(array) {
+// function sortingNumbers(array) {
 
-    // array.sort((a, b) => a - b); // сортиране в нарастващ ред
-    // array.sort((a, b) => b - a); // сортиране в намаляващ ред
+// array.sort((a, b) => a - b); // сортиране в нарастващ ред
+// array.sort((a, b) => b - a); // сортиране в намаляващ ред
 
-    // let result = [];
+// let result = [];
 
-    // while (array.length) {
-    //     // взима се най-малкото
-    //     result.push(array.shift());
+// while (array.length) {
+//     // взима се най-малкото
+//     result.push(array.shift());
 
-    //     // взима се най-голямото
-    //     if (array.length) {
-    //         result.push(array.pop());
-    //     }
-    // }
+//     // взима се най-голямото
+//     if (array.length) {
+//         result.push(array.pop());
+//     }
+// }
 
-    // return result;
+// return result;
 
-    //------------------------------------------------------
+//------------------------------------------------------
 
-    // array.sort((a, b) => a - b); // сортиране в нарастващ ред
-    // let result = [];
+// array.sort((a, b) => a - b); // сортиране в нарастващ ред
+// let result = [];
 
-    // // Two pointers
-    // let left = 0;
-    // let right = array.length - 1;
+// // Two pointers
+// let left = 0;
+// let right = array.length - 1;
 
-    // for (let i = 0; i < array.length; i++) {
+// for (let i = 0; i < array.length; i++) {
 
-    //     if (i % 2 === 0) {
-    //         result.push(array[left]);
-    //         left++;
+//     if (i % 2 === 0) {
+//         result.push(array[left]);
+//         left++;
 
-    //     } else {
-    //         result.push(array[right]);
-    //         right--;
-    //     }
+//     } else {
+//         result.push(array[right]);
+//         right--;
+//     }
 
-    // }
-    // console.log("[" + result.join(", ") + "]");
+// }
+// console.log("[" + result.join(", ") + "]");
 
-    // return result;
-
-
-    //---------------------------------------------------
-
-    // array.sort((a, b) => a - b); // сортиране в нарастващ ред
-   
-    // let rightStartingPoint = Math.ceil(array.length / 2);
-    // const result = Array.from({ length: array.length }, () => 0);
-
-    // for (let i = 0; i < array.length; i++) {
-
-    //     if (i < rightStartingPoint) {
-    //         result[i * 2] = array[i];
-
-    //     } else {
-    //         result[(array.length - i - 1) * 2 + 1] = array[i];
-    //     }
-    // }
-
-    // console.log("[" + result.join(", ") + "]");
+// return result;
 
 
-    // return result;
+//---------------------------------------------------
+
+// array.sort((a, b) => a - b); // сортиране в нарастващ ред
+
+// let rightStartingPoint = Math.ceil(array.length / 2);
+// const result = Array.from({ length: array.length }, () => 0);
+
+// for (let i = 0; i < array.length; i++) {
+
+//     if (i < rightStartingPoint) {
+//         result[i * 2] = array[i];
+
+//     } else {
+//         result[(array.length - i - 1) * 2 + 1] = array[i];
+//     }
+// }
+
+// console.log("[" + result.join(", ") + "]");
+
+
+// return result;
+// }
+
+
+// function sortingNumbers(arrayOfNumbers) {  NOOOOOO!!!!!
+
+//     let result = arrayOfNumbers
+//         .sort((a, b) => b - a)
+//         .reduce((acc, number, index) => {
+
+//             if (index < arrayOfNumbers.length / 2) {
+//                 acc.push(arrayOfNumbers[arrayOfNumbers.length - 1 - index]);
+//                 acc.push(number);
+//             }
+
+//             return acc;
+//         }, []);
+
+//     console.log("[" + result.join(", ") + "]");
+
+//     return result;
+// }
+
+
+
+function sortingNumbers(arrayOfNumbers) {
+
+   let length = arrayOfNumbers.length;
+
+    let result = arrayOfNumbers
+        .sort((a, b) => b - a)
+        .slice(0, Math.ceil(length / 2))
+        .reduce((acc, number, index) => {
+            acc.push(arrayOfNumbers.pop());
+
+            if (index < Math.floor(length / 2)) {
+                acc.push(number);
+            }
+
+            return acc;
+        }, []);
+
+    return result;
 }
 
 
-
-sortingNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);
+sortingNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18]);
