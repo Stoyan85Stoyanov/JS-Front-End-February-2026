243b66481deb6b1e09ed1b706be06987b5c92290
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-09-09 18:53:14 +0300

    js

--- Exercise_2/05.js
+++ Exercise_2/05.js
@@ -1,30 +1,58 @@
+// function revealWords(listOfWords, text) {
+
+// 1. Text search -> cannot match !!! exactly !!! 5 asterisks
+
+// let words = listOfWords.split(", ").sort((a, b) => b.length - a.length);
+
+// for (const word of words) {
+
+//     let teplate = Array.from({ length: word.length }, () => "*").join("");
+//     text = text.replaceAll(teplate, word);
+// }
+// console.log(text);
+
+
+
+// 2. Regex search
+
+// let words = listOfWords.split(", ");
+
+// for (const word of words) {
+//     const pattern =  new RegExp(`(?<!\\*)\\*{${word.length}}(?!\\*)`, "g");
+//     text = text.replaceAll(pattern, word);
+// }
+
+// console.log(text);
+
+// }
+
+
 function revealWords(listOfWords, text) {
-    
-    // 1. Text search -> cannot match !!! exactly !!! 5 asterisks
+    let words = listOfWords.split(', ');
+    let templates = text.split(' ');
 
-    // let words = listOfWords.split(", ").sort((a, b) => b.length - a.length);
-
-    // for (const word of words) {
-
-    //     let teplate = Array.from({ length: word.length }, () => "*").join("");
-    //     text = text.replaceAll(teplate, word);
-    // }
-    // console.log(text);
+    for (let i = 0; i < templates.length; i++) {
+        let template = templates[i];
 
 
+        if (template.includes('*')) {
 
-    // 2. Regex search
+            for (let j = 0; j < words.length; j++) {
+                let word = words[j];
 
-    let words = listOfWords.split(", ");
-
-    for (const word of words) {
-        const pattern =  new RegExp(`(?<!\\*)\\*{${word.length}}(?!\\*)`, "g");
-        text = text.replaceAll(pattern, word);
+                if (word.length === template.length) {
+                    templates[i] = word;
+                    words.splice(j, 1);
+                    break;
+                }
+            }
+        }
     }
 
-    console.log(text);
+    console.log(templates.join(' '));
 }
 
+revealWords('great, learning', '***** is ********');
 revealWords('great', 'softuni is ***** place for learning new programming languages');
 revealWords('great, learning', 'softuni is ***** place for ******** new programming languages');
 
