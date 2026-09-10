1e6e1083a262de21d2e7a75eed8fed03c79437d0
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-09-10 22:32:43 +0300

    js

--- Exercise_2/05.js
+++ Exercise_2/05.js
@@ -27,29 +27,49 @@
 // }
 
 
-function revealWords(listOfWords, text) {
-    let words = listOfWords.split(', ');
-    let templates = text.split(' ');
+// function revealWords(listOfWords, text) {
+//     let words = listOfWords.split(', ');
+//     let templates = text.split(' ');
 
-    for (let i = 0; i < templates.length; i++) {
-        let template = templates[i];
+//     for (let i = 0; i < templates.length; i++) {
+//         let template = templates[i];
 
 
-        if (template.includes('*')) {
+//         if (template.includes('*')) {
 
-            for (let j = 0; j < words.length; j++) {
-                let word = words[j];
+//             for (let j = 0; j < words.length; j++) {
+//                 let word = words[j];
 
-                if (word.length === template.length) {
-                    templates[i] = word;
-                    words.splice(j, 1);
-                    break;
-                }
-            }
-        }
+//                 if (word.length === template.length) {
+//                     templates[i] = word;
+//                     words.splice(j, 1);
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+
+//     console.log(templates.join(' '));
+// }
+
+
+function revealWords(wordInput, text) {
+
+    const words = wordInput.split(', ');
+
+    const sortedWords = words.sort((a, b) => a.length - b.length);
+
+    for (const word of sortedWords) {
+        const searchWord = '*'.repeat(word.length);
+
+        // text = text.replaceAll(searchWord, word);
+
+        text = text.replace(searchWord, (match, offset) =>
+            text[offset - 1] === '*' || text[offset + word.length] === '*' ? match : word
+        );
     }
 
-    console.log(templates.join(' '));
+    console.log(text);
 }
 
 revealWords('great, learning', '***** is ********');
