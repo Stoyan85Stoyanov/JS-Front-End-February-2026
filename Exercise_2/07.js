895075f9c9c72f8ea2d6dce55c273a3afc1e8273
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-09-15 16:45:42 +0300

    js

--- Exercise_2/07.js
+++ Exercise_2/07.js
@@ -21,9 +21,28 @@
     //     console.log(`${word} not found!`);
     // }
 
+    function stringSubstring(words, text) {
+
+    const isIncluded = text
+        .split(' ')
+        .map(word => word.toLowerCase())
+        .includes(words);
+
+    if (isIncluded) {
+        console.log(words);
+        
+    } else {
+        console.log(`${words} not found!`);
+    }
+
+}
+
+
 //     function stringSubstring(word, text) {
 
-//     if (text.toLowerCase().includes(word.toLowerCase())) {
+//         const pattern = new RegExp(`\\b${word}\\b`, 'i');
+
+//     if (pattern.test(text)) {
 //         return console.log(word);
 
 //     } else {
@@ -31,20 +50,6 @@
 //     }
 
 // }
-
-
-    function stringSubstring(word, text) {
-
-        const pattern = new RegExp(`\\b${word}\\b`, 'i');
-
-    if (pattern.test(text)) {
-        return console.log(word);
-
-    } else {
-        console.log(`${word} not found!`);
-    }
-
-}
 
 stringSubstring('java', 'C# is the best programming language');
 stringSubstring('javascript', 'JavaScript is the best programming language');
