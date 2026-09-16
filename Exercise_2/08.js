66a2d89deaa42d1114f73b192671c54af00f4723
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-09-16 17:06:22 +0300

    js

--- Exercise_2/08.js
+++ Exercise_2/08.js
@@ -1,12 +1,50 @@
-function pascalCaseSplitter(text) {
+// function pascalCaseSplitter(text) {
 
-    const pattern = /[A-Z][a-z]*/gm;
+//     const pattern = /[A-Z][a-z]*/gm;
 
-    let words = Array.from(text.matchAll(pattern));
+//     let words = Array.from(text.matchAll(pattern));
 
-    console.log(words.join(", "));
+//     console.log(words.join(", "));
 
+// }
+
+
+// function pascalCaseSplitter(singleString) {
+
+//     const pattern = /[A-Z][a-z]*/gm;
+
+//     let words = Array.from(singleString.matchAll(pattern));
+
+//     console.log(words.join(", "));
+
+// }
+
+
+
+function pascalCaseSplitter(singleString) {
+
+    let currentWord = singleString[0];
+    let words = [];
+
+    for (let i = 1; i < singleString.length; i++) {
+
+        const currentCharacter = singleString[i];
+
+        if (currentCharacter === currentCharacter.toUpperCase()) {
+            words.push(currentWord);
+            currentWord = currentCharacter;
+
+        } else {
+            currentWord += currentCharacter;
+            continue;
+        }
+    }
+
+    words.push(currentWord);
+
+    console.log(words.join(', '));
 }
+
 
 pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
 pascalCaseSplitter('HoldTheDoor');
