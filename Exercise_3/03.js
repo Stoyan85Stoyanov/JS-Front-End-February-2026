bc3cbff2096eeb0ed9c1ef13c13632ac6cb2ca32
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-09-21 16:51:07 +0300

    js

--- Exercise_3/03.js
+++ Exercise_3/03.js
@@ -1,44 +1,44 @@
 
-/**
-    * @param {string} charOne
-    * @param {string} charTwo
-    */
+// /**
+//     * @param {string} charOne
+//     * @param {string} charTwo
+//     */
 
-function charactersInRange(charOne, charTwo) {
+// function charactersInRange(charOne, charTwo) {
 
-    /**
-        * @param {number} from
-        * @param {number} to
-        */
+//     /**
+//         * @param {number} from
+//         * @param {number} to
+//         */
 
-    function getCharactersBetween(from, to) {
+//     function getCharactersBetween(from, to) {
 
-        const result = [];
+//         const result = [];
 
-        for (let i = from + 1; i < to; i++) {
-            result.push(String.fromCodePoint(i));
+//         for (let i = from + 1; i < to; i++) {
+//             result.push(String.fromCodePoint(i));
 
-        }
+//         }
 
-        return result.join(" ");
-    }
+//         return result.join(" ");
+//     }
 
-    const charFirst = charOne.codePointAt(0);
-    const charSecond = charTwo.codePointAt(0);
+//     const charFirst = charOne.codePointAt(0);
+//     const charSecond = charTwo.codePointAt(0);
 
-    let outputResult;
+//     let outputResult;
 
-    if (charFirst < charSecond) {
+//     if (charFirst < charSecond) {
 
-        outputResult = getCharactersBetween(charFirst, charSecond);
+//         outputResult = getCharactersBetween(charFirst, charSecond);
 
-    } else {
+//     } else {
 
-        outputResult = getCharactersBetween(charSecond, charFirst);
-    }
+//         outputResult = getCharactersBetween(charSecond, charFirst);
+//     }
 
-    console.log(outputResult);
-}
+//     console.log(outputResult);
+// }
 
 // let start = Math.min(char1.charCodeAt(0), char2.charCodeAt(0));
 // let end = Math.max(char1.charCodeAt(0), char2.charCodeAt(0));
@@ -50,6 +50,57 @@ function charactersInRange(charOne, charTwo) {
 // }
 
 // console.log(result.join(' '));
+
+
+
+function charactersInRange(firstCharacter, secondCharacter) {
+
+
+    const rangeCharacter = getCharacterRange(
+        getLowerCharacter(firstCharacter, secondCharacter),
+        getHigherCharacter(firstCharacter, secondCharacter));
+
+    console.log(rangeCharacter.join(' '));
+
+
+    function getLowerCharacter(first, second) {
+        if (first < second) {
+            return first;
+
+        } else {
+            return second;
+        }
+
+    }
+
+    function getHigherCharacter(first, second) {
+        if (first > second) {
+            return first;
+
+        } else {
+            return second;
+        }
+
+    }
+
+
+    function getCharacterRange(lowerCharacter, higherCharacter) {
+
+        let rangeCharacter = [];
+
+        let lowerCharacterCode = lowerCharacter.charCodeAt(0);
+        let higherCharacterCode = higherCharacter.charCodeAt(0);
+
+        for (let i = lowerCharacterCode + 1; i < higherCharacterCode; i++) {
+            let character = String.fromCodePoint(i);
+
+            rangeCharacter.push(character)
+        }
+
+        return rangeCharacter;
+    }
+
+}
 
 
 charactersInRange('a', 'd');
