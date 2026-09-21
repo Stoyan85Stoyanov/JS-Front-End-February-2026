6c3a7a484d3bc3351d54536b8cc0a70c6318566a
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-09-21 12:15:25 +0300

    js

--- Exercise_3/02.js
+++ Exercise_3/02.js
@@ -1,19 +1,39 @@
-function addAndSubtract(numberOne, numberTwo, numberThree) {
+// function addAndSubtract(numberOne, numberTwo, numberThree) {
 
-    function sum(numberOne, numberTwo) {
-        return numberOne + numberTwo;
+//     function sum(numberOne, numberTwo) {
+//         return numberOne + numberTwo;
+//     }
+
+//     function subtract(sum, numberThree) {
+//         return sum - numberThree;
+//     }
+
+//     // let sumResult = sum(numberOne, numberTwo);
+//     // let finalResult = subtract(sumResult, numberThree);
+
+//     let finalResult = subtract(sum(numberOne, numberTwo), numberThree);
+
+//     console.log(finalResult);
+
+// }
+
+
+
+function addAndSubtract(firstNumber, secondNumber, thirdNumber) {
+
+
+    function sum(firstNumber, secondNumber) {
+        return firstNumber + secondNumber;
     }
 
-    function subtract(sum, numberThree) {
-        return sum - numberThree;
+    function subtract(sumResultFunction, thirdNumber) {
+        return sumResultFunction - thirdNumber;
     }
 
-    // let sumResult = sum(numberOne, numberTwo);
-    // let finalResult = subtract(sumResult, numberThree);
+    const sumResult = sum(firstNumber, secondNumber);
+    const subtractResult = subtract(sumResult, thirdNumber);
 
-    let finalResult = subtract(sum(numberOne, numberTwo), numberThree);
-
-    console.log(finalResult);
+    console.log(subtractResult);
 
 }
 
