d04fa7254a5fda52cd9b268ffab0dc926b740f54
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-09-24 19:52:35 +0300

    js

--- Exercise_3/07.js
+++ Exercise_3/07.js
@@ -1,21 +1,36 @@
+// function N_x_N_Matrix(number) {
+
+//     function getRowAndColumn() {
+//         let result = [];
+
+//         for (let i = 0; i < number; i++) {
+//             result.push(number);
+//         }
+
+//         return result;
+//     }
+
+
+//     for (let i = 0; i < number; i++) {
+//         console.log(getRowAndColumn().join(" "));
+//     }
+
+// }
+
 function N_x_N_Matrix(number) {
 
-    function getRowAndColumn() {
-        let result = [];
+    for (let row = 1; row <= number; row++) {
+        printNxN_Matrix();
 
-        for (let i = 0; i < number; i++) {
-            result.push(number);
-        }
-
-        return result;
     }
 
-
-    for (let i = 0; i < number; i++) {
-        console.log(getRowAndColumn().join(" "));
+    function printNxN_Matrix() {
+        // console.log(`${number} `.repeat(number));
+        const result = (number + ' ').repeat(number).trim();
+        console.log(result);
     }
-    
 }
+
 
 N_x_N_Matrix(3);
 N_x_N_Matrix(7);
