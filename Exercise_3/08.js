fa639ae7738b108cbf2c7acf544e6ff9d9478ce4
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-09-25 16:58:40 +0300

    js

--- Exercise_3/08.js
+++ Exercise_3/08.js
@@ -1,45 +1,69 @@
+// function perfectNumber(number) {
+
+//     function divisors() {
+//         let result = [];
+//         let sqrtNumber = Math.sqrt(number);
+
+//         for (let i = 1; i < sqrtNumber; i++) {
+
+//             if (number % i === 0) {
+//                 result.push(i);
+//                 result.push(number / i);
+//             }
+//         }
+
+//         // Ако корен квадратен е цяло число !!!
+//         if (sqrtNumber === Math.trunc(sqrtNumber)) {
+//             result.push(sqrtNumber);
+//         }
+
+//         return result;
+//     }
+
+
+//     function sum(array) {
+//         let sum = 0;
+
+//         for (const element of array) {
+//             sum += element;
+//         }
+
+//         return sum;
+//     }
+
+
+//     function numberPerfect() {
+//         let perfect = 2 * number === sum(divisors());
+//         return perfect;
+//     }
+
+
+//     console.log(numberPerfect() ? "We have a perfect number!" : "It's not so perfect.");
+// }
+
 function perfectNumber(number) {
 
-    function divisors() {
-        let result = [];
-        let sqrtNumber = Math.sqrt(number);
+    const properDivisors = [];
 
-        for (let i = 1; i < sqrtNumber; i++) {
-
-            if (number % i === 0) {
-                result.push(i);
-                result.push(number / i);
-            }
+    for (let divisor = 1; divisor < number; divisor++) {
+        if (number % divisor === 0) {
+            properDivisors.push(divisor);
         }
-
-        // Ако корен квадратен е цяло число !!!
-        if (sqrtNumber === Math.trunc(sqrtNumber)) {
-            result.push(sqrtNumber);
-        }
-
-        return result;
     }
 
+    const properDivisorsSum = sumNumbers(properDivisors);
 
-    function sum(array) {
-        let sum = 0;
+    console.log(properDivisorsSum === number
+        ? "We have a perfect number!" : "It's not so perfect.");
 
-        for (const element of array) {
-            sum += element;
-        }
 
-        return sum;
+    function sumNumbers(numbers) {
+        const sumProperPositiveDivisors = numbers.reduce((acc, number) => acc + number, 0);
+
+        return sumProperPositiveDivisors;
     }
-
-
-    function numberPerfect() {
-        let perfect = 2 * number === sum(divisors());
-        return perfect;
-    }
-
-
-    console.log(numberPerfect() ? "We have a perfect number!" : "It's not so perfect.");
 }
+
 
 perfectNumber(81);
 perfectNumber(6);
