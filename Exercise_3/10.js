8b70d9a9898e4fd230612cf78fd25d3aaa1ec7f5
StoyanStoyanov085 <stoyan.stoyanov.kz@mail.bg>
2026-09-26 16:41:17 +0300

    js

--- Exercise_3/10.js
+++ Exercise_3/10.js
@@ -1,20 +1,43 @@
+// function factorialDivision(numberOne, numberTwo) {
+
+//     function factorialNumber(number) {
+
+//         if (number === 0 || number === 1) {
+//             return 1;
+
+//         }
+
+//         let resultNumber = number * factorialNumber(number - 1);
+//         return resultNumber;
+//     }
+
+//     let result = factorialNumber(numberOne) / factorialNumber(numberTwo);
+//     console.log(result.toFixed(2));
+
+// }
+
+
 function factorialDivision(numberOne, numberTwo) {
-
-    function factorialNumber(number) {
-
-        if (number === 0 || number === 1) {
-            return 1;
-
-        }
-
-        let resultNumber = number * factorialNumber(number - 1);
-        return resultNumber;
-    }
 
     let result = factorialNumber(numberOne) / factorialNumber(numberTwo);
     console.log(result.toFixed(2));
 
+
+    function factorialNumber(number) {
+
+        let factorial = 1;
+
+        for (let factorNumber = 2; factorNumber <= number; factorNumber++) {
+            factorial *= factorNumber;
+            
+        }
+
+        return factorial;
+    }
+
 }
+
+
 
 factorialDivision(5, 2);
 factorialDivision(6, 2);
